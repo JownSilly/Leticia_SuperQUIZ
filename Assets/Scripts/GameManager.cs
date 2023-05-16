25e3982ad128ee2a1bc7fdde23b74bb5e7384521
JownSilly <joaovitordealmeidaojeda2@hotmail.com>
2023-05-16 16:12:25 -0400

    att - novasQuestoes ao Clicar ou endTime

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,18 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using System;
 
 public class GameManager : MonoBehaviour
 {
+    [Header("Questions")]
     [SerializeField] private PerguntasSO currentQuestion;
+    [SerializeField] private PerguntasSO[] allQuestionScriptableObj;
     [SerializeField] private TextMeshProUGUI statementText;
     [SerializeField] private GameObject[] alternativeTextTMP;
-    [Header ("sprites")]
+    [Header ("Sprites")]
+    [SerializeField] private Sprite SpriteStandardAlternative;
     [SerializeField] private Sprite SpriteRightAlternative;
     [SerializeField] private Sprite SpriteWrongAlternative;
-
+    [Header("Controller")]
+    private Timer timer;
+    private int questionPosition;
+    private int correctly_Answers;
+    [SerializeField] private TextMeshProUGUI EndGameMessageTXT;
+    [SerializeField] private GameObject[] screens;
     void Start()
     {
+        screens[0].SetActive(true);
+        screens[1].SetActive(false);
+        timer = GetComponent<Timer>();
+        timer.RegistrarParada(OnParadaTimer);
+        
+        questionPosition = 0;
+        correctly_Answers = 0;
+
         statementText.SetText(currentQuestion.GetStatements());
         string[] alternatives = currentQuestion.GetAlternatives();
         for(int i = 0; i < alternatives.Length; i++)
@@ -26,19 +43,20 @@ public class GameManager : MonoBehaviour
     //Gerencia as opçoes selecionadas
     public void HandleOption(int selectedAlternative)
     {
-        Image imageAlt = alternativeTextTMP[selectedAlternative].GetComponent<Image>();
+        DisableOptionButtons();
+        PararTimer();
         int rightAnswer = currentQuestion.GetRightAnswer();
-        Image imageAltRight = alternativeTextTMP[rightAnswer].GetComponent<Image>();
+        Image imageAlt = alternativeTextTMP[selectedAlternative].GetComponent<Image>();
         if (selectedAlternative == rightAnswer)
         {
             ChangeButtonSprite(imageAlt, SpriteRightAlternative);
-            DisableOptionButtons();
+            correctly_Answers++;
             Debug.Log("ganhou");
         }
         else
         {
+            Image imageAltRight = alternativeTextTMP[rightAnswer].GetComponent<Image>();
             ChangeButtonSprite(imageAlt, SpriteWrongAlternative);
-            DisableOptionButtons();
             ChangeButtonSprite(imageAltRight, SpriteRightAlternative);
             Debug.Log("perdeu");
         }
@@ -51,10 +69,72 @@ public class GameManager : MonoBehaviour
             alternativeTextTMP[i].GetComponent<Button>().enabled = false;
         }
     }
+    public void DefaultOptionButtons()
+    {
+        for (int i = 0; i < alternativeTextTMP.Length; i++)
+        {
+            alternativeTextTMP[i].GetComponent<Button>().enabled = true;
+        }
+        for (int i = 0; i < alternativeTextTMP.Length; i++)
+        {
+            alternativeTextTMP[i].GetComponent<Image>();
+            ChangeButtonSprite(alternativeTextTMP[i].GetComponent<Image>(), SpriteStandardAlternative);
+        }
+        timer.Zerar();
+    }
     //troca o sprite dos botoes
     public void ChangeButtonSprite(Image image, Sprite sprite)
     {
         image.sprite = sprite;
+    }
+    void PararTimer()
+    {
+        timer.Parar();
+    }
+    private void OnParadaTimer()
+    {
+        Debug.Log(questionPosition);
+        if (questionPosition >= allQuestionScriptableObj.Length - 1)
+        {
+            Invoke("EndGame", 0.5f);
+        }
+        else
+            Invoke("NewQuestion", 0.5f);
+    }
+    void GenerateQuestion()
+    {
+        currentQuestion = allQuestionScriptableObj[questionPosition];
+
+        statementText.SetText(currentQuestion.GetStatements());
+        string[] alternatives = currentQuestion.GetAlternatives();
+        for (int i = 0; i < alternatives.Length; i++)
+        {
+            // Coloca os texto das alternativas nos TextMeshPro que sao filho de cada um dos botoes de alternativas.
+            alternativeTextTMP[i].GetComponentInChildren<TextMeshProUGUI>().SetText(alternatives[i]);
+        }
+    }
+    void NewQuestion()
+    {
+        questionPosition++;
+        GenerateQuestion();
+        DefaultOptionButtons();
+        timer.Zerar();
+    }
+    public void RestartGame()
+    {
+        questionPosition = 0;
+        correctly_Answers = 0;
+        DefaultOptionButtons();
+        GenerateQuestion();
+        timer.Zerar();
+        screens[0].SetActive(true);
+        screens[1].SetActive(false);
+    }
+    void EndGame()
+    {
+        EndGameMessageTXT.SetText("Você acertou " + correctly_Answers + " questões de " + allQuestionScriptableObj.Length + "\n Parabéns!!! ");
+        screens[1].SetActive(true);
+        screens[0].SetActive(false);
     }
     void Update()
     {
--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -4,28 +4,56 @@ using UnityEngine;
 using UnityEngine.UI;
 public class Timer : MonoBehaviour
 {
-    [SerializeField] private float maxTime;
-    private float currentTime;
-    private Slider slider;
+    // Delegate
+    // Delegates são um tipo de dados que representam referências a um ou mais métodos 
+    public delegate void OnParadaTimer();
+    public OnParadaTimer onParadaTimer;
 
-    // Start is called before the first frame update
+    [SerializeField] private float maxTime;
+    [SerializeField] private Slider slider;
+    private float currentTime;
+    private bool isCount;
+
     void Start()
     {
         currentTime = 0f;
-        slider = GetComponent<Slider>();
         slider.maxValue = maxTime;
         slider.value = currentTime;
+        isCount = true;
     }
-
-    // Update is called once per frame
     void Update()
     {
-        currentTime += 1 * Time.deltaTime;
-        if (currentTime > maxTime)
+        if (isCount)
         {
-            currentTime = 0f;
-            Debug.Log("EndTime");
+            currentTime += 1 * Time.deltaTime;
+            slider.value = currentTime;
+
+            if (currentTime > maxTime && onParadaTimer != null)
+            {
+                Parar();
+            }
         }
+    }
+    // Registro do método para o delegate
+    public void RegistrarParada(OnParadaTimer metodo)
+    {
+        onParadaTimer += metodo;
+    }
+
+    public void Parar()
+    {
+        isCount = false;
+
+        if (onParadaTimer != null)
+        {
+            onParadaTimer();
+        }
+    }
+
+    public void Zerar()
+    {
+        currentTime = 0f;
         slider.value = currentTime;
+        isCount = true;
     }
 }
